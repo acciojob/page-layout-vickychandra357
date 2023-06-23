cdcd151e1148b0f7160d1b89290d751f46e9d983
tonystark <chandravicky288@gmail.com>
2023-06-23 10:43:02 +0530

    change in page layout

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
-const PageLayout = ({ pageLayout}) => {
-  return (
-    <p className='pageLayout'>{pageLayout}</p>
-  );
+function PageLayout  ({ pageLayout})  {
+  return   <p className='pageLayout'>{pageLayout}</p>
+  
 };
 
 export default PageLayout;
